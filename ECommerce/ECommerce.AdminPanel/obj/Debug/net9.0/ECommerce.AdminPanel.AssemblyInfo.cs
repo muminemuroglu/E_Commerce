31f29cbd8258cc0f8d37bf4791c88f5e17f6b5e7
muminemuroglu <muminemuroglu@outlook.com>
2026-01-27 18:31:14 +0300

    Search

--- ECommerce/ECommerce.AdminPanel/obj/Debug/net9.0/ECommerce.AdminPanel.AssemblyInfo.cs
+++ ECommerce/ECommerce.AdminPanel/obj/Debug/net9.0/ECommerce.AdminPanel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.AdminPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e9968e9527935c28f78867845f5eb81d2ab6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ea62a08d922e28109c0cc6281a421ac87f9cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.AdminPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.AdminPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECommerce/ECommerce.ClientApplication/src/app/layouts/navbar/navbar.component.ts
+++ ECommerce/ECommerce.ClientApplication/src/app/layouts/navbar/navbar.component.ts
@@ -1,17 +1,36 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router'; // Router Eklendi
+import { FormsModule } from '@angular/forms'; // FormsModule Eklendi
 import { AuthService } from '../../core/services/auth-service.service';
 import { CartService } from '../../core/services/cart-service.service';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule], // FormsModule'u eklemeyi unutma!
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class Navbar {
-  // HTML'de direkt erişmek için servisi public yapıyoruz
-  constructor(public authService: AuthService, public cartService: CartService) {}
+  
+  searchKeyword: string = ''; // Arama metnini tutacak değişken
+
+  constructor(
+    public authService: AuthService, 
+    public cartService: CartService,
+    private router: Router // Router inject edildi
+  ) {}
+
+  onSearch() {
+    if (this.searchKeyword && this.searchKeyword.trim().length > 0) {
+      // Ürünler sayfasına 'keyword' parametresiyle git
+      this.router.navigate(['/products'], { 
+        queryParams: { keyword: this.searchKeyword } 
+      });
+      // Arama sonrası kutuyu temizlemek istersen:
+       this.searchKeyword = ''; 
+    }
+  }
 }
